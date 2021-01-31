e935874a6a7f6fb47375f5dd3adbb26248ac9202
sebbe <13065474+sebastianappler@users.noreply.github.com>
2021-01-31 13:11:04 +0100

    Added keybindings for tabs

--- config.el
+++ config.el
@@ -80,6 +80,19 @@
   :ensure t
   :config (treemacs-icons-dired-mode))
 
+(use-package! centaur-tabs
+  :bind
+  ("C-c t s" . centaur-tabs-counsel-switch-group)
+  ("C-c t w" . centaur-tabs--kill-this-buffer-dont-ask)
+  ("C-c t k" . centaur-tabs-kill-all-buffers-in-current-group)
+  ("C-c t o" . centaur-tabs-kill-other-buffers-in-current-group)
+  ("C-c t k" . centaur-tabs-forward)
+  ("C-c t j" . centaur-tabs-backward)
+  ("C-c t >" . centaur-tabs-move-current-tab-to-right)
+  ("C-c t 0" . centaur-tabs-select-beg-tab)
+  ("C-c t $" . centaur-tabs-select-end-tab)
+)
+
 ;;
 ;;; UI
 
