58aca2db3b8d69691ab0d667ca997b7567fc92a0
sebbe <13065474+sebastianappler@users.noreply.github.com>
2021-04-10 15:54:30 +0200

    Added lsp config

---
+++ lisp/lsp.el
@@ -0,0 +1,4 @@
+;; To avoid asking for restart when switching workspace in treemacs
+(setq lsp-restart 'ignore)
+(setq lsp-enable-symbol-highlighting 1)
+;; (add-hook! 'lsp-mode lsp-toggle-symbol-highlight)
