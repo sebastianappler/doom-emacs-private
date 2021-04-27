1228bbada89d2150fb0b042df625e3af9f660020
sebbe <13065474+sebastianappler@users.noreply.github.com>
2021-04-27 22:47:52 +0200

    Add light modeline and zen
    This commit will add the lighter version
    of modeline to make Emacs cleaner. Also
    installs zen mode for super cozy programming
    sessions.

--- init.el
+++ init.el
@@ -37,9 +37,9 @@
        ;;indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
-       modeline            ; snazzy, Atom-inspired modeline, plus API
-       ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
+       (modeline +light)            ; snazzy, atom-inspired modeline, plus api
+       nav-flash         ; blink cursor line after big motions
        ophints             ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        tabs                ; a tab bar for Emacs
@@ -49,7 +49,7 @@
        vi-tilde-fringe     ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
        workspaces          ; tab emulation, persistence & separate workspaces
-       ;;zen               ; distraction-free coding or writing
+       zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
