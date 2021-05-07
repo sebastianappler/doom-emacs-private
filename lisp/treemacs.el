59b536929352d14eec8d9ba77e2d23caeec043e3
sebbe <13065474+sebastianappler@users.noreply.github.com>
2021-05-07 22:44:28 +0200

    Add global keybind to switch workspace
    This commit will add a global key bind
    so that it's possible to switch workspace
    when treemacs is not targeted. Will also
    expand and open to the switched project.

--- lisp/treemacs.el
+++ lisp/treemacs.el
@@ -6,6 +6,7 @@
 
 ;; Treemacs
 (use-package! treemacs
+  :defer t
   :init
     (define-key treemacs-mode-map [mouse-1] #'treemacs-single-click-expand-action)
   :config
@@ -14,19 +15,26 @@
       treemacs-indentation                      1
       treemacs-width                            32
       treemacs-workspace-switch-cleanup         'all
+      lsp-treemacs-sync-mode                    1
     )
    ;; (treemacs-git-mode -1)
   )
   :bind
   (:map global-map
-     ("M-0"     . treemacs-select-window)))
+     ("M-0"             .       treemacs-select-window)
+     ("C-c C-w s"             .       treemacs-switch-workspace)
+     ))
 
+(add-hook! counsel-bookmark
+  (treemacs-display-current-project-exclusively))
 
 (defun treemacs-expand-when-first-used (&optional visibility)
   (when (or (null visibility) (eq visibility 'none))
     (treemacs-do-for-button-state
      :on-root-node-closed (treemacs-toggle-node)
-     :no-error t)))
+     :no-error t)
+    ))
 
 (add-hook! 'treemacs-select-functions #'treemacs-expand-when-first-used)
 (add-hook! 'treemacs-switch-workspace-hook #'treemacs-expand-when-first-used)
+(add-hook! 'treemacs-switch-workspace-hook #'treemacs-select-window)
