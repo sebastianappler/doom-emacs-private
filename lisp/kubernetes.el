1704d63f043f39353f3f16334189de9731a1a421
Sebastian Appler <13065474+sebastianappler@users.noreply.github.com>
2022-01-12 21:06:34 +0100

    Correctly load kubernetes-evil. "q" kills buffer

--- lisp/kubernetes.el
+++ lisp/kubernetes.el
@@ -1,14 +1,10 @@
 ;;; lisp/kubernetes.el -*- lexical-binding: t; -*-
 
 (use-package! kubernetes
-  :ensure t
-  :commands (kubernetes-overview)
-  :config
-  (setq kubernetes-poll-frequency 3600
-        kubernetes-redraw-frequency 3600))
+  :commands (kubernetes-overview))
 
 (use-package! kubernetes-evil
-  :ensure t
-  :after kubernetes)
-
-(evil-set-initial-state 'kubernetes-mode 'insert)
+  :after (kubernetes-overview)
+  :config
+  (evil-define-key 'motion kubernetes-mode-map
+    (kbd "q")    #'kill-this-buffer))
