b3d3992a980cb04087a658a60bfe4f0738fd8bff
Sebastian Appler <sebastian.applerolsson@spinit.se>
2022-01-12 15:13:26 +0100

    Default insert for evil command window mode

--- lisp/evil.el
+++ lisp/evil.el
@@ -2,3 +2,11 @@
 
 (after! evil-snipe
   (evil-snipe-mode -1))
+
+;; Avoids annoying behaviour where pressing "q" and ":" in
+;; sequence opens a Command Line window that will block your
+;; commands as visual mode is default.
+;; If you e.g. try yo close a window with "q" and then type
+;; ":bd<RET>" you would have to type ":ibd<RET>" to enter
+;; insert mode for the Command Line window
+(evil-set-initial-state 'evil-command-window-mode 'insert)
