e93d4e5af19e8343f41cb2a00b049b5d43871a4c
Sebastian Appler <13065474+sebastianappler@users.noreply.github.com>
2022-01-12 21:52:18 +0100

    Remove unused lisp/window.el functions

--- lisp/windows.el
+++ lisp/windows.el
@@ -1,15 +1,6 @@
-;;; windows.el --- Working with windows within frames -*- lexical-binding: t -*-
-;;; Commentary:
+;;; lisp/windows.el -*- lexical-binding: t; -*-
 
-;; This is not about the "Windows" OS, but rather Emacs's "windows"
-;; concept: these are the panels within an Emacs frame which contain
-;; buffers.
-
-;;; Code:
-
-;;----------------------------------------------------------------------------
 ;; When splitting window, show (other-buffer) in the new window
-;;----------------------------------------------------------------------------
 (defun split-window-func-with-other-buffer (split-function)
   (lambda (&optional arg)
     "Split this window and switch to the new window unless ARG is provided."
@@ -22,45 +13,6 @@
 
 (global-set-key (kbd "C-x 2") (split-window-func-with-other-buffer 'split-window-vertically))
 (global-set-key (kbd "C-x 3") (split-window-func-with-other-buffer 'split-window-horizontally))
-
-(defun sanityinc/toggle-delete-other-windows ()
-  "Delete other windows in frame if any, or restore previous window config."
-  (interactive)
-  (if (and winner-mode
-           (equal (selected-window) (next-window)))
-      (winner-undo)
-    (delete-other-windows)))
-
-(global-set-key (kbd "C-x 1") 'sanityinc/toggle-delete-other-windows)
-
-
-;; Borrowed from http://postmomentum.ch/blog/201304/blog-on-emacs
-(defun sanityinc/split-window()
-  "Split the window to see the most recent buffer in the other window.
-Call a second time to restore the original window configuration."
-  (interactive)
-  (if (eq last-command 'sanityinc/split-window)
-      (progn
-        (jump-to-register :sanityinc/split-window)
-        (setq this-command 'sanityinc/unsplit-window))
-    (window-configuration-to-register :sanityinc/split-window)
-    (switch-to-buffer-other-window nil)))
-
-(global-set-key (kbd "<f7>") 'sanityinc/split-window)
-
-
-
-(defun sanityinc/toggle-current-window-dedication ()
-  "Toggle whether the current window is dedicated to its current buffer."
-  (interactive)
-  (let* ((window (selected-window))
-         (was-dedicated (window-dedicated-p window)))
-    (set-window-dedicated-p window (not was-dedicated))
-    (message "Window %sdedicated to %s"
-             (if was-dedicated "no longer " "")
-             (buffer-name))))
-
-(global-set-key (kbd "C-c <down>") 'sanityinc/toggle-current-window-dedication)
 
 (defun toggle-frame-split ()
   "If the frame is split vertically, split it horizontally or vice versa.
