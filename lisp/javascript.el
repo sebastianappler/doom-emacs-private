959ade0f4dcb08f634285b4307923a79540ff2a7
Sebastian Appler <13065474+sebastianappler@users.noreply.github.com>
2022-03-05 22:08:14 +0100

    Add .dir-locals example for yarn pnp

--- lisp/javascript.el
+++ lisp/javascript.el
@@ -1,5 +1,10 @@
 ;;; lisp/javascript.el -*- lexical-binding: t; -*-
 
-
 (setq-hook! 'typescript-mode-hook +format-with-lsp nil)
 (setq-hook! 'typescript-tsx-mode-hook +format-with-lsp nil)
+
+;; .dir-locals.el example to make ts-ls work with yarn pnp
+;; You need to run `yarn dlx @yarnpkg/sdks bas` to generate the .yarn/sdks folder
+;; ((typescript-tsx-mode . ((eval . (let ((project-directory(car (dir-locals-find-file default-directory))))
+;;                (setq lsp-clients-typescript-server-args
+;;                      `("--tsserver-path" ,(concat project-directory ".yarn/sdks/typescript/bin/tsserver") "--stdio")))))))
