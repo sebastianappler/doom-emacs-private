3b83b104620f55e6523e1a5a8cae7e1f77f48eb4
Sebastian Appler <13065474+sebastianappler@users.noreply.github.com>
2022-04-08 23:15:35 +0200

    kubernetes: Fix popup & keybindings

--- lisp/kubernetes.el
+++ lisp/kubernetes.el
@@ -1,10 +1,18 @@
 ;;; lisp/kubernetes.el -*- lexical-binding: t; -*-
 
 (use-package! kubernetes
-  :commands (kubernetes-overview))
+  :commands (kubernetes-overview)
+  :config
+  (set-popup-rules!
+    '(("^\\*kubernetes overview" :ignore t))))
 
 (use-package! kubernetes-evil
   :after (kubernetes-overview)
   :config
-  (evil-define-key 'motion kubernetes-mode-map
-    (kbd "q")    #'kill-this-buffer))
+  (map!
+   :map kubernetes-mode-map
+   :m "n"       #'kubernetes-set-namespace
+   :m "C-j"     #'magit-section-forward
+   :m "C-k"     #'magit-section-backward
+   :m "]"       #'magit-section-forward-sibling
+   :m "["       #'magit-section-backward-sibling))
