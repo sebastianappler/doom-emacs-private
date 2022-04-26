cf0bddd711fd65ca41db2c94cda9daba04555fb0
Sebastian Appler <13065474+sebastianappler@users.noreply.github.com>
2022-04-26 23:05:26 +0200

    kubernetes: Fix kubernetes evil

--- lisp/kubernetes.el
+++ lisp/kubernetes.el
@@ -9,13 +9,13 @@
     '(("^\\*kubernetes overview" :ignore t))))
 
 (use-package! kubernetes-evil
-  :defer t
-  :after (kubernetes-overview)
-  :config
-  (map!
+  :after (kubernetes-overview))
+
+(map!
+   :after kubernetes-evil
    :map kubernetes-mode-map
    :m "n"       #'kubernetes-set-namespace
    :m "C-j"     #'magit-section-forward
    :m "C-k"     #'magit-section-backward
    :m "]"       #'magit-section-forward-sibling
-   :m "["       #'magit-section-backward-sibling))
+   :m "["       #'magit-section-backward-sibling)
