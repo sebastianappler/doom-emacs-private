bedd7d65f14464f9ba7952b617f17d379b048b9e
Sebastian Appler <13065474+sebastianappler@users.noreply.github.com>
2022-08-23 22:34:21 +0200

    dashboard: insert org agenda in agenda widget

--- config.el
+++ config.el
@@ -71,7 +71,23 @@
 (use-package! dashboard
   :ensure t
   :config
-  (setq dashboard-center-content t)
+  (setq dashboard-center-content t
+        dashboard-items '((agenda   . 5)
+                          (recents  . 5)
+                          (projects . 5)))
+
+  (defun dashboard-get-agenda()
+    "Get copy of org-agenda buffer."
+    (save-window-excursion
+      (org-agenda-list)
+      (prog1 (buffer-string)
+        (kill-buffer))))
+
+  (defun dashboard-insert-agenda (&rest _)
+    "Insert a copy of org-agenda buffer."
+    (dashboard-insert-heading "Agenda for today:" "a")
+    (dolist (line (split-string (dashboard-get-agenda) "\n"))
+      (insert "\n    " line)))
 
   (dashboard-setup-startup-hook))
 
