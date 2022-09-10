da3b85be78411531409701d1c143faf4af309272
Sebastian Appler <13065474+sebastianappler@users.noreply.github.com>
2022-09-10 22:46:13 +0200

    dashboard: switch to dashboard agenda

--- lisp/dashboard.el
+++ lisp/dashboard.el
@@ -5,8 +5,8 @@
   :config
   (setq dashboard-center-content t
         dashboard-items '((agenda   . 5)
-                          ;; (recents  . 5)
-                          ;; (projects . 5)
+                          (recents  . 5)
+                          (projects . 5)
                           )
         dashboard-startup-banner (concat doom-user-dir "assets/doom-emacs-gray.png")
         dashboard-banner-logo-title nil
@@ -15,17 +15,17 @@
 
   ;; Shows org agenda on dashboard
   ;;
-  (defun dashboard-get-agenda()
-    "Get copy of org-agenda buffer."
-    (save-window-excursion
-      (org-agenda-list)
-      (prog1 (buffer-string)
-        (kill-buffer))))
+  ;; (defun dashboard-get-agenda()
+  ;;   "Get copy of org-agenda buffer."
+  ;;   (save-window-excursion
+  ;;     (org-agenda-list)
+  ;;     (prog1 (buffer-string)
+  ;;       (kill-buffer))))
 
-  (defun dashboard-insert-agenda (&rest _)
-    "Insert a copy of org-agenda buffer."
-    (dashboard-insert-heading "Agenda for today:")
-    (dolist (line (split-string (dashboard-get-agenda) "\n"))
-      (insert "\n" line)))
+  ;; (defun dashboard-insert-agenda (&rest _)
+  ;;   "Insert a copy of org-agenda buffer."
+  ;;   (dashboard-insert-heading "Agenda for today:")
+  ;;   (dolist (line (split-string (dashboard-get-agenda) "\n"))
+  ;;     (insert "\n" line)))
 
   (dashboard-setup-startup-hook))
