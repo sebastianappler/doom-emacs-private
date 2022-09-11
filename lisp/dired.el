ebcfb949ed0a52358e790b7f67b7c5ee10843a27
Sebastian Appler <13065474+sebastianappler@users.noreply.github.com>
2022-09-11 09:47:31 +0200

    dired: set all-the-icons for dirvish

--- lisp/dired.el
+++ lisp/dired.el
@@ -14,7 +14,7 @@
   :config
   (set-popup-rule! "^ ?\\*Dirvish.*" :ignore t)
   (setq dirvish-cache-dir (concat doom-cache-dir "dirvish/")
-        dirvish-attributes '(vc-state file-size vscode-icon collapse subtree-state)
+        dirvish-attributes '(vc-state file-size all-the-icons collapse subtree-state)
         dirvish-mode-line-format
         '(:left (sort file-time symlink) :right (omit yank index))
         dired-listing-switches
