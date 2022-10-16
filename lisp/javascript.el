3b06148ed8b50a3b25cfedcbb6429229ef169d83
Sebastian Appler <13065474+sebastianappler@users.noreply.github.com>
2022-10-16 10:33:55 +0200

    javascript: reduce lag when open ts[x]/js[x] files

--- lisp/javascript.el
+++ lisp/javascript.el
@@ -8,6 +8,10 @@
 (setq-hook! 'typescript-mode-hook +format-with-lsp nil)
 (setq-hook! 'typescript-tsx-mode-hook +format-with-lsp nil)
 
+;; Reduces lag when opening js[x]/ts[x] files
+;; See https://github.com/doomemacs/doomemacs/issues/6878
+(advice-add #'add-node-modules-path :override #'ignore)
+
 ;; .dir-locals.el example to make ts-ls work with yarn pnp
 ;; You need to run `yarn dlx @yarnpkg/sdks bas` to generate the .yarn/sdks folder
 ;; ((typescript-tsx-mode . ((eval . (let ((project-directory(car (dir-locals-find-file default-directory))))
