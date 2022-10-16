9f2b908f5c56fba3e63c466d8dba86402971a77c
Sebastian Appler <13065474+sebastianappler@users.noreply.github.com>
2022-10-16 10:36:06 +0200

    mu4e: update mail settings

--- lisp/mu4e.el
+++ lisp/mu4e.el
@@ -3,14 +3,22 @@
 (use-package! mu4e
   :defer t
   :config
-  (set-email-account! "mail.com"
-                      '((mu4e-sent-folder       . "/mail.com/Sent Mail")
-                        (mu4e-drafts-folder     . "/mail.com/Drafts")
-                        (mu4e-trash-folder      . "/mail.com/Trash")
-                        (mu4e-refile-folder     . "/mail.com/All Mail")
-                        (smtpmail-smtp-user     . "user@mail.com")
-                        (mu4e-compose-signature . "---\nBest regards"))
+  (set-email-account! "protonmail.com"
+                      '((mu4e-sent-folder       . "/protonmail.com/Sent Mail")
+                        (mu4e-drafts-folder     . "/protonmail.com/Drafts")
+                        (mu4e-trash-folder      . "/protonmail.com/Trash")
+                        (mu4e-refile-folder     . "/protonmail.com/All Mail")
+                        (mu4e-compose-signature . "\n/Sebastian"))
                       t)
+
+  (setq message-send-mail-function 'smtpmail-send-it
+        smtpmail-auth-credentials "~/.authinfo.gpg"
+        smtpmail-smtp-server "127.0.0.1"
+        smtpmail-stream-type 'starttls
+        smtpmail-smtp-service 1025
+        smtpmail-debug t)
+
   (setq mu4e-maildir-shortcuts
-        '((:maildir "/mail.com/INBOX"     :key  ?i)
-          (:maildir "/mail.com/All Mail"  :key  ?a))))
+        '((:maildir "/protonmail.com/INBOX"      :key  ?i)
+          (:maildir "/protonmail.com/All Mail"   :key  ?a)
+          (:maildir "/protonmail.com/Sent Mail"  :key  ?s))))
