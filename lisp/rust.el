ae2a4d33243f06d9483e762212df391edfebcc00
Sebastian Appler <13065474+sebastianappler@users.noreply.github.com>
2022-11-15 19:26:23 +0100

    rust: Remove cppdbg debug template

--- lisp/rust.el
+++ lisp/rust.el
@@ -1,9 +1,7 @@
 ;;; lisp/rust.el -*- lexical-binding: t; -*-
 
 (after! dap-mode
-  ;;(require 'dap-cpptools)
   (require 'dap-gdb-lldb)
-  (setq dap-auto-configure-features '(sessions locals tooltip))
   (dap-register-debug-template "Rust::GDB Run Configuration"
                                (list :type "gdb"
                                      :request "launch"
@@ -14,19 +12,6 @@
                                      :dap-compilation "cargo build"
                                      :dap-compilation-dir "${workspaceFolder}"
                                      ))
-
- (dap-register-debug-template "Rust::CppTools Run Configuration"
-                                 (list :type "cppdbg"
-                                       :request "launch"
-                                       :name "Rust::Run"
-                                       :MIMode "gdb"
-                                       :miDebuggerPath "rust-gdb"
-                                       :environment []
-                                       :program "${workspaceFolder}/target/debug/${workspaceFolderBasename}"
-                                       :cwd "${workspaceFolder}"
-                                       :console "external"
-                                       :dap-compilation "cargo build"
-                                       :dap-compilation-dir "${workspaceFolder}"))
 
   (add-hook! 'dap-terminated-hook (dap-delete-all-sessions))
   (add-hook! 'dap-stopped-hook (run-at-time "200 milliseconds" nil #'dap-ui-locals)))
