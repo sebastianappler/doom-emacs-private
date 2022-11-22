c376dab64defdccd42cd08b223b696c3ff798c3f
Sebastian Appler <13065474+sebastianappler@users.noreply.github.com>
2022-11-22 20:03:37 +0100

    org(projects): Add org project capture template

--- lisp/org.el
+++ lisp/org.el
@@ -4,32 +4,71 @@
 (setq org-directory "~/org/"
       org-roam-directory (concat org-directory "notes/")
       org-roam-db-location (concat org-roam-directory ".org-roam.db")
-      org-agenda-include-diary t)
+      org-agenda-include-diary t
+      org-time-stamp-formats '("[%Y-%m-%d %a %H:%M]"))
 
-;;
-;; Ox hugo
 (after! org
   (setq org-capture-templates
         (append
         '(("h" "Hugo post" entry (file+olp "./blog/posts.org" "Blog")
-           (function org-hugo-new-subtree-post-capture-template)))
-        org-capture-templates)
-  )
-)
+          (function appler/org-hugo-capture-template))
+          ("P" "Add project" plain
+          (file appler/org-project-generate-filename)
+          "%(appler/org-project-capture-template)")
+          ("T" "Add task" entry (file+olp buffer-name "Project" "Tasks" "Backlog")
+          (function appler/org-project-add-task-template))
 
-(with-eval-after-load 'org-capture
-  (defun org-hugo-new-subtree-post-capture-template ()
+          )
+        org-capture-templates))
+
+  (defun appler/org-hugo-capture-template ()
     (let* ((title (read-from-minibuffer "Post Title: "))
-    (fname (org-hugo-slug title)))
+           (filename (org-hugo-slug title)))
       (mapconcat #'identity `(
         ,(concat "* TODO " title)
         ":PROPERTIES:"
-        ,(concat ":EXPORT_FILE_NAME: " fname)
+        ,(concat ":EXPORT_FILE_NAME: " filename)
         ,(concat ":EXPORT_DATE: " (format-time-string "%Y-%m-%d"))
-        ;;":EXPORT_DESCRIPTION: "
-        ;;":EXPORT_HUGO_CUSTOM_FRONT_MATTER: "
         ":END:"
         "%?\n")
-        "\n"))
-    )
-)
+        "\n")))
+
+  (defun appler/org-project-capture-template ()
+    (let* ((name appler-org-project-name)
+           (filename appler-org-project-filename))
+      (mapconcat #'identity `(
+        ,(concat "#+title: " name)
+        ""
+        ,"Project"
+        ":PROPERTIES:"
+        ,(concat ":NAME: " name)
+        ":CATEGORY: Project"
+        ,(concat ":CREATED: " (format-time-string "[%Y-%m-%d %a %H:%M]"))
+        ":END:"
+        ""
+        "** Description"
+        "%?"
+        "*** Goals"
+        ""
+        "** Tasks"
+        "*** Backlog"
+        ""
+        "*** Active"
+        "*** Closed"
+        ""
+        "** Outcome")
+        "\n")))
+
+  (defun appler/org-project-add-task-template ()
+      (mapconcat #'identity `(
+        ,"* TODO %?")
+        ":PROPERTIES:"
+        ,(concat ":CREATED: " (format-time-string "[%Y-%m-%d %a %H:%M]"))
+        ,(concat ":CUSTOM_ID: " 1)
+        ":END:")
+        "\n")
+
+  (defun appler/org-project-generate-filename ()
+    (setq appler-org-project-name (read-from-minibuffer "Project name: "))
+    (setq appler-org-project-filename (org-hugo-slug appler-org-project-name))
+    (expand-file-name (format "%s.org" appler-org-project-filename) "~/org/projects")))
